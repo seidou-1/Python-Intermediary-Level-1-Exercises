0cb31beac836c89f117ebe3103af81f9cce0b097
MB <mbarry323@gmail.com>
2018-07-20 10:21:52 -0400

    continuing a loop

--- Exercises/Loops.py
+++ Exercises/Loops.py
@@ -10,20 +10,26 @@ def main():
     while (x<5):
         print (x)
         x +=1
-
+ 
 #For loop:
 for x in range(0, 10):
     print(x)
-
+ 
 #For loop to iterate over a collection:
 weekdays = ["Monday","Tuesday","Wednesday", "Thursday", "Friday"]
 for d in weekdays:
     print (d)
-    
+     
 #Breaking out of a for loop:
 for x in range(0, 10):
     print (x)
     if (x==5): break
+
+#Continuing a loop (aka skip):
+for x in range(0, 10):    
+    if (x % 2 == 0): continue
+    print (x)
+    
 
 if __name__ == "__main__":
     main()
